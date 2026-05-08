0733ae26b195af61df6a48a4e42b87c52a9b9eb0
Faishal Mumtazah <faishal@students.amikom.ac.id>
2026-05-08 07:53:42 +0700

    CRUD Tabel event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -1,14 +1,15 @@
 <?php
 
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
+
 
 class Event extends Model
 {
-    use HasFactory;
-
+    //
     protected $fillable = [
         'category_id',
         'title',
@@ -20,7 +21,7 @@ class Event extends Model
         'poster_path'
     ];
 
-    // Relasi balik ke Category
+
     public function category()
     {
         return $this->belongsTo(Category::class);
