c7f1af80b2d6670cf2cce0517e05ef24fa1c2932
Faishal Mumtazah <faishal@students.amikom.ac.id>
2026-05-08 07:42:19 +0700

    Update 2026_04_24_033003_create_events_table.php

--- database/migrations/2026_04_24_033003_create_events_table.php
+++ database/migrations/2026_04_24_033003_create_events_table.php
@@ -13,15 +13,6 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
-            // Foreign Key relasi one-to-many ke categories
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->string('title');
-            $table->text('description')->nullable();
-            $table->dateTime('date');
-            $table->string('location');
-            $table->integer('price');
-            $table->integer('stock');
-            $table->string('poster_path')->nullable();
             $table->timestamps();
         });
     }
